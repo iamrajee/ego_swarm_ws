516ec5194bc448c9f69f0b3740cb7aa32a033097
Rajendra Singh <singh.raj1997@gmail.com>
2024-01-16 18:26:47 +0530

    new comm update

--- code/EGO-Planner-v2/swarm-playground/main_ws/src/planner/plan_manage/scripts/move_in_gazebo.py
+++ code/EGO-Planner-v2/swarm-playground/main_ws/src/planner/plan_manage/scripts/move_in_gazebo.py
@@ -6,6 +6,26 @@ from gazebo_msgs.srv import SpawnModel, SetModelState, GetModelState
 from std_msgs.msg import Int16, Bool
 import sys
 
+
+import math
+
+def generate_circular_trajectory(radius, num_points):
+  theta = 0
+  points = []
+  for i in range(num_points):
+    x = radius * math.cos(theta)
+    y = radius * math.sin(theta)
+    points.append((x, y))
+    theta += 2 * math.pi / num_points  # Increment angle for next point
+  return points
+
+radius = 3
+num_points = 25  # Adjust for desired number of points
+trajectory = generate_circular_trajectory(radius, num_points)
+trajectory_itr=0
+
+
+
 drone_id = sys.argv[1]
 
 pub = rospy.Publisher('/move_command', Pose, queue_size=1)  # Create publisher for move_cb
@@ -13,11 +33,16 @@ pub = rospy.Publisher('/move_command', Pose, queue_size=1)  # Create publisher f
 drone_cords_offset = [[0.5,0.5,0.5],[0.5,-0.5,0.5],[-0.5,0.5,0.5],[-0.5,-0.5,0.5]]
 
 #drone1->drone2->drone3->drone4->drone1...
+
 number_of_drones=4
 drone_id_of_interest=(int(drone_id)%number_of_drones) + 1
 
 
 def update_position():
+    
+    global num_points
+    global trajectory
+    global trajectory_itr
     # if msg.header.frame_id == "null":
     #     return
 
@@ -28,11 +53,20 @@ def update_position():
     
     model_state_of_interest.pose.position.x += model_state_of_interest.pose.position.x + (drone_cords_offset[int(drone_id) - 1][0] - drone_cords_offset[drone_id_of_interest - 1][0])
     model_state_of_interest.pose.position.y += model_state_of_interest.pose.position.y + (drone_cords_offset[int(drone_id) - 1][1] - drone_cords_offset[drone_id_of_interest - 1][1])
-    model_state_of_interest.pose.position.z += model_state_of_interest.pose.position.z + (drone_cords_offset[int(drone_id) - 1][2] - drone_cords_offset[drone_id_of_interest - 1][2])
+    model_state_of_interest.pose.position.z=0.5#+= model_state_of_interest.pose.position.z + (drone_cords_offset[int(drone_id) - 1][2] - drone_cords_offset[drone_id_of_interest - 1][2])
 
     model_state = ModelState()
     model_state.model_name = "drone_" + drone_id
     model_state.pose = model_state_of_interest.pose #msg.pose.pose
+        
+    if int(drone_id)==1:
+        trajectory_itr=trajectory_itr%num_points
+        x,y = trajectory[trajectory_itr]
+        model_state.pose.position.x = x
+        model_state.pose.position.y = y
+        model_state.pose.position.z = 0.5
+        print(x,y)  
+        trajectory_itr+=1      
 
     rospy.wait_for_service('/gazebo/set_model_state')
     set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
@@ -50,9 +84,10 @@ def main():
 
     # pub_odom = rospy.Publisher('/drone_' + drone_id + '_visual_slam/odom', Odometry)
     while not rospy.is_shutdown():
+        print("--------------drone:", drone_id)
         update_position()
-        rospy.sleep(5)
-
+        # rospy.sleep(0.1)
+        
     rospy.spin()
 
 if __name__ == '__main__':
